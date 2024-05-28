971aca5813ab94034cb05d584fd243471cb463db
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-05-28 16:24:56 +0700

    remove deprecated params

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ dotnev.config({ path: './config.env' });
 
 const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose
-    .connect(db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection successful!'));
+mongoose.connect(db).then(() => console.log('DB connection successful!'));
 
 const app = express();
 
