892ba0c419f50aba771a0bc04c66de33496604ed
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-05-28 16:51:35 +0700

    script to import,delete data to database

---
+++ dev-data/data/import-sample-data.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+// models
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(db).then(() => console.log('DB connection successful!'));
+
+// read file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// function to import/delete data into database
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
