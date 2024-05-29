5185015945e4daa1d4ed9ceb3611725ba88ec0ab
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-05-29 17:32:15 +0700

    query data from db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,48 @@ exports.getTour = async (req, res) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        //--basic query
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields']; // excude mongoDB query operators
+        excludedFields.forEach((el) => delete queryObj[el]);
+
+        //--advanced query
+        // const products = await Product.find({ price: { $gte: 50, $lte: 100 } }); for range
+        // const products = await Product.find({ price: { $in: [50, 100] } }); for exact match (multiple values)
+        // const products = await Product.find({ price: 50 }); for exact match (single value)
+
+        const queryStr = JSON.stringify(queryObj).replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        let query = Tour.find(JSON.parse(queryStr));
+
+        //--sorting results
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        //--limiting fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v'); // field for tracking versioning of a document of a collection in MongoDB
+        }
+
+        //--pagniation
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 10;
+        const skip = (page - 1) * limit;
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page does not exist');
+        }
+        query = query.skip((page - 1) * limit).limit(limit);
+
+        const tours = await query;
 
         res.status(200).json({
             status: 'success',
