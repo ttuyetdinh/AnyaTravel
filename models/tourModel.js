70a5fefb7f317bda46d9b390efdbfb9ed9b32f00
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-05-30 17:28:02 +0700

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,70 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+        },
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a group size'],
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty'],
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4.5,
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price'],
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            trim: true,
+            required: [true, 'A tour must have a summary'],
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have a cover image'],
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            select: false, // Hide this field from the output
+        },
+        startDates: [Date],
     },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration'],
+    {
+        // add virtual properties when converting to JSON or Object
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
     },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5,
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a summary'],
-    },
-    description: {
-        type: String,
-        trim: true,
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false, // Hide this field from the output
-    },
-    startDates: [Date],
+);
+
+// Virtual properties
+tourSchema.virtual('durationWeeks').get(function () {
+    const weeks = Math.trunc(this.duration / 7);
+    const days = this.duration % 7;
+    return `${weeks} weeks and ${days} days`;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
