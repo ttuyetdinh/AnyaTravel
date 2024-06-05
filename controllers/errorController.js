f82e393e15443bb5ab585197f2450cf9a5922769
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-05 17:56:33 +0700

    remove unuse code

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -46,11 +46,6 @@ exports.globalErrorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
