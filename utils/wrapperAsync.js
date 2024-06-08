7150e08e4338b9d4fffe7077e8859a3d6a417bfb
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-08 19:09:55 +0700

    explain for wrapper Async

--- utils/wrapperAsync.js
+++ utils/wrapperAsync.js
@@ -6,3 +6,12 @@ module.exports = wrapperAsync =
         const next = args[args.length - 1];
         return Promise.resolve(func).catch(next);
     };
+
+// Fully syntax version
+// const wrapperrAsync = function (fn) {
+//     return function (...args) {
+//         const func = fn(...args);
+//         const next = args[args.length - 1];
+//         return Promise.resolve(func).catch((error) => next(error));
+//     };
+// };
