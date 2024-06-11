2796ec929791c347c6d94101da88bcd6e52899fc
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-11 17:17:39 +0700

    basic crud for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,47 @@
 const User = require('../models/userModel');
 const wrapperAsync = require('../utils/wrapperAsync');
+const tools = require('../utils/tools');
 
+//  -----------------------user routes-----------------------
+exports.getProfile = wrapperAsync(async (req, res) => {
+    const user = await User.findById(req.user.id);
+
+    const filteredUser = tools.filterFields(user.toObject(), ['name', 'email', 'photo'], 'include', true);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: filteredUser,
+        },
+    });
+});
+exports.updateProfile = wrapperAsync(async (req, res) => {
+    const filteredBody = tools.filterFields(req.body, ['name', 'email', 'photo']);
+
+    const user = await User.findById(req.user.id);
+    Object.assign(user, filteredBody);
+    await user.save({ validateModifiedOnly: true });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: user,
+        },
+    });
+});
+
+exports.suspendProfile = wrapperAsync(async (req, res) => {
+    const user = await User.findById(req.user.id);
+    user.active = false;
+    await user.save({ validateModifiedOnly: true });
+
+    res.status(200).json({
+        status: 'success',
+        data: null,
+    });
+});
+
+//  -----------------------admin routes-----------------------
 exports.getAllUsers = wrapperAsync(async (req, res) => {
     const users = await User.find();
 
