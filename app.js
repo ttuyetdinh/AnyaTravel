80b9986d1e309c42eb1cc09246977487be9c5988
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-12 16:35:39 +0700

    limit request from same Ip, add security HTTP headers

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoute');
 const tourRouter = require('./routes/tourRoute');
 const { globalErrorHandler } = require('./controllers/errorController');
 
+const { limiterConfig } = require('./configurations/ipLimitConfiguration');
+
 const app = express();
 
-// Middleware
+//---------------- Global Middleware ----------------
+// Set security HTTP headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from the same IP
+app.use('/api', limiterConfig);
+
+// Body parser, reading data from body into req.body, limit the body to 100kb
+app.use(express.json({ limit: '100kb' }));
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
