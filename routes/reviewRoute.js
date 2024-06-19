39394af4600c90374e25a169165d96a8faa08ae2
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-19 17:18:46 +0700

    implement basic crud for review

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -4,11 +4,22 @@ const router = express.Router({ mergeParams: true });
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-// POST /tour/234fad4/reviews
-// POST /reviews
+// crud /tour/234fad4/reviews
+// crud /reviews
 router
     .route('/')
     .get(reviewController.getAllReviews)
-    .post(authController.authorize, authController.restrictTo('user', 'admin'), reviewController.createReview);
+    .post(
+        authController.authorize,
+        authController.restrictTo('user', 'admin'),
+        reviewController.setTourUserIds,
+        reviewController.createReview,
+    );
+
+router
+    .route('/:id')
+    .get(reviewController.getReview)
+    .patch(authController.authorize, authController.restrictTo('user', 'admin'), reviewController.updateReview)
+    .delete(authController.authorize, authController.restrictTo('user', 'admin'), reviewController.deleteReview);
 
 module.exports = router;
